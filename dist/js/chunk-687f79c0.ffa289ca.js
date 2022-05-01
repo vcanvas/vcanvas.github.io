0b9fedf798f5ec2d11e6f5d8b98edbd5cb5455ec
vcanvas <104730149+vcanvas@users.noreply.github.com>
2022-05-02 00:59:29 +0800

    Delete dist directory

--- dist/js/chunk-687f79c0.ffa289ca.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-687f79c0"],{1966:function(t,n,e){},"2def":function(t,n,e){"use strict";e.r(n);var c=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{attrs:{id:"Page404"}},[e("el-empty",{attrs:{description:"您所查找的页面不存在，请检查URL地址后重试！"}})],1)},i=[],s={},u=s,r=(e("4420"),e("2877")),a=Object(r["a"])(u,c,i,!1,null,null,null);n["default"]=a.exports},4420:function(t,n,e){"use strict";e("1966")}}]);
-//# sourceMappingURL=chunk-687f79c0.ffa289ca.js.map
